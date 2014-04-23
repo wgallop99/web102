d48d92cacdf6257c3ddd593052576ea2380fba38
Craig <Craig@CPC>
2014-04-23 19:36:44 -0400

    Added password validation to reg.js and attatched the necessary HTML elements to the reg.js file for later insertion into reg.html

--- javascripts/reg.js
+++ javascripts/reg.js
@@ -6,10 +6,30 @@ function submit()
 	
 	
 function valditate()
-	var password
-	var confirmPassword
-
+{
+	var pass1=document.getElementById('pass1');
+	var pass2=document.getElementById('pass2');
+	var message=document.getElementById('confirm');
+	var matchColor="#2c9c49";
+	var missColor="#ff6666";
 	
+	if (pass1.value==pass2.value)
+	{
+		pass2.style.backgroundColor=matchColor;
+		message.style.color=matchColor;
+		message.innerHTML="&#x2713 Passwords Match!";
+	}	
+	else 
+	{	
+		pass2.style.backgroundColor=missColor;
+		message.style.color=missColor;
+		message.innerHTML="&#x1f6ab Passwords do not match!";
+	}
+}//end of function validate	
+
+//Will need the following inserted into the HTML code to make the function work inside of the retype password. 
+// id="pass1"    ;          id="pass2" onkeyup="checkPass(); return false;"
+//  <span id="message" class="message"></span>
 function error()
 	var firstName
 	var lastName
