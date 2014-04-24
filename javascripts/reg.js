6a4bd770fbf33904d126ccde809e17a8dc0767d5
Craig <Craig@CPC>
2014-04-23 20:46:55 -0400

    Slight modifications to reg.js to make validatePass work with HTML

--- javascripts/reg.js
+++ javascripts/reg.js
@@ -5,31 +5,35 @@ function submit()
 	var password
 	
 	
-function valditate()
-{
-	var pass1=document.getElementById('pass1');
-	var pass2=document.getElementById('pass2');
-	var message=document.getElementById('confirm');
-	var matchColor="#2c9c49";
-	var missColor="#ff6666";
-	
-	if (pass1.value==pass2.value)
+	function validatePass()
 	{
-		pass2.style.backgroundColor=matchColor;
-		message.style.color=matchColor;
-		message.innerHTML="&#x2713 Passwords Match!";
-	}	
-	else 
-	{	
-		pass2.style.backgroundColor=missColor;
-		message.style.color=missColor;
-		message.innerHTML="&#x1f6ab Passwords do not match!";
-	}
-}//end of function validate	
 
-//Will need the following inserted into the HTML code to make the function work inside of the retype password. 
-// id="pass1"    ;          id="pass2" onkeyup="checkPass(); return false;"
-//  <span id="message" class="message"></span>
+	    var pass1 = document.getElementById('pass1');
+	    var pass2 = document.getElementById('pass2');
+	        var message = document.getElementById('confirmMessage');
+	    var goodColor = "#2c9c49";
+	    var badColor = "#ff6666";
+	   
+	    if(pass1.value == pass2.value)
+	    {
+	       
+	        pass2.style.backgroundColor = goodColor;
+	        message.style.color = goodColor;
+	        message.innerHTML = "&#x2713 Passwords Match!";
+	    }else{
+	        
+	        pass2.style.backgroundColor = badColor;
+	        message.style.color = badColor;
+	        message.innerHTML = "&#x1f6ab Passwords Do Not Match!";
+	    }
+	}  //End of function validatePass
+// Will need the following code in HTML to run
+// <label for="pass1">Password:</label>
+// <input type="password" name="pass1" id="pass1">
+
+// <label for="pass2">Confirm Password:</label>
+// <input type="password" name="pass2" id="pass2" onkeyup="validatePass(); return false;">
+//  <span id="confirmMessage" class="confirmMessage"></span>
 function error()
 	var firstName
 	var lastName
