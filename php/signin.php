c602d38489ca112ba20137f7918321eef5788f13
Carly Messmer <carly.messmer@gmail.com>
2014-04-29 18:34:12 -0400

    Added php sign in notes

--- php/signin.php
+++ php/signin.php
@@ -1,17 +1,28 @@
 <?php
 
-$email = $GET['email'];
-            $password = $GET['password'];
+$email = $GET['$email'];
+$password = $GET['password'];
 
             if ( (empty($username)) || (empty($password)) ) {
                 header('Location: Login.php?fail=1');
                 die();
             }
-            if ( ((strlen($username)) >25) || ((strlen($password)) >25) ) {
+            if ( ) {
                 header('Location: Login.php?fail=2');
                 die();
             }
-            if ( (preg_match("/[^\w-.]/", $username)) ) {
-                header('Location: Login.php?fail=3');
-                die();
+            if (  ) {
+              
             }
+
+<?php
+
+// Grab User submitted information
+$email = $_POST["users_email"];
+$password1 = $_POST["users_pass"];
+
+if($row["users_email"]==$email && $row["users_pass"]==$pass)
+    echo"You are a validated user.";
+else
+    echo"Sorry, your credentials are not valid, Please try again.";
+?>
