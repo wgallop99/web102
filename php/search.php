99c9ff09a3af44ff8c5f3a0ea49fa8adadce22d7
ccravens <chad.cravens@ossys.com>
2014-05-02 18:26:47 -0400

    Added SQL script to create user table. Added search code in search.php.

--- php/search.php
+++ php/search.php
@@ -1,1 +1,22 @@
-http://php.about.com/od/phpwithmysql/ss/php_search_4.htm
+<?php
+        $last_name = $_GET['LastName'];
+
+        $email = "";
+
+        $con = mysql_connect("iweb.ossys.com","web102","PUR5tr3ngth","web102");
+        if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+        mysql_select_db("web102");
+
+        $result = mysql_query("SELECT email FROM user WHERE last_name LIKE '%$last_name%'");
+        if(!$result) {
+                echo mysql_error();
+        } else {
+                while($row = mysql_fetch_array($result)){
+                        $email = $row['email'];
+                }
+        }
+
+        echo $email;
+?>
