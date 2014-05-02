a9c2ae1f3a3faa3e501151945f76cc43a7e75f4c
Carly Messmer <carly.messmer@gmail.com>
2014-05-02 18:51:42 -0400

    Updated signin php

--- php/signin.php
+++ php/signin.php
@@ -2,10 +2,29 @@
 
 // Grab User submitted information
 $email = $_POST["login_email"];
-$password1 = $_POST["login_password"];
+$password = $_POST["login_password"];
 
-if($row["users_email"]==$email && $row["users_pass"]==$pass)
-    echo"You are a validated user.";
-else
-    echo"Sorry, your credentials are not valid, Please try again.";
+        $con = mysql_connect("iweb.ossys.com","web102","PUR5tr3ngth","web102");
+        if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+        mysql_select_db("web102");
+		
+		$password2 = "";
+        
+        $result = mysql_query("SELECT password FROM user WHERE email = '$email'");
+        if(!$result) {
+                echo mysql_error();
+        } else {
+                while($row = mysql_fetch_array($result)){
+                        $password2 = $row['password'];
+                }
+        }
+
+	if($password==$password2) {
+    	echo"You are a validated user.";
+    	}
+	else {
+    	echo"Sorry, your credentials are not valid, Please try again.";
+    	}
 ?>
