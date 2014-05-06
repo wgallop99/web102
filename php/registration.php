a530127da21ad203451eecb2c6f6c191b7bb7458
ccravens <chad.cravens@ossys.com>
2014-05-06 17:50:50 -0400

    Completed registration code.

--- php/registration.php
+++ php/registration.php
@@ -1,50 +1,26 @@
 <?php
-	$firstname = "";
-	$lastname = "";
-	$username = "";
-	$email = "";
-	$password1 = "";
-	$password2 = "";
 
-if ((isset($_GET['Fname'] && |isempty($_GET['Fname'])) && (isset($_GET['Lname'] && |isempty$_GET['Lname'])) && (isset($_GET['Username'] && |isempty$_GET['Username'])) && (isset($_GET['Password'] && |isempty$_GET['Password'])) && (isset($_GET['password2'] && |isempty$_GET['password2'])) && (isset($_GET['email'] && |isempty$_GET['email']))
+if((!isset($_POST['Fname']) && !isempty($_POST['Fname'])) && 
+	(!isset($_POST['Lname']) && !isempty($_POST['Lname'])) && 
+	(!isset($_POST['Username']) && !isempty($_POST['Username'])) && 
+	(!isset($_POST['password2']) && !isempty($_POST['password2'])) && 
+	(!isset($_POST['Email']) && !isempty($_POST['Email']))) {
+	echo "You must fill out all fields, go back and try again!";
+} else {
+	
+	    $con = mysql_connect("iweb.ossys.com","web102","PUR5tr3ngth","web102");
+        if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+        mysql_select_db("web102");
 
+        $result = mysql_query("INSERT INTO user (first_name, last_name, username, password, email) VALUES ($_POST['Fname'], $_POST['Lname'], $_POST['Username'], $_POST['password2'], $_POST['Email'])");
+        if(!$result) {
+                echo mysql_error();
+        } else {
+        	echo "You successfully registered!";
+        }
+	
+}
 
-if (($firstname=isset & $firstname=="") && ($lastname==null || $lastname=="") && ($email==null || $email=="") && ($password1==null || $password1=="") && ($password2==null || $password2==""))
-	{
-	echo "All fields must be filled out";
-	return false;
-	}	
-if ($firstname==null || $firstname=="");
-	{
-	echo "Fill in first name";
-	return false;
-	}
-if ($lastname==null	|| $lastname=="");
-	{
-	echo "Fill in last name";
-	return false;
-	}
-if	($email ==null || $email=="");
-	{
-	echo "Fill in email";
-	return false;
-	}
-if ($password1==null || $password1=="");
-	{
-	echo "Fill in password";
-	return false;
-	}
-if ($password2==null || $password2=="");
-	{
-	echo "Verify password";
-	return false;
-	}	
-if ($_GET['$password1'] != $_GET['$password2'])
-	{
-	echo "Passwords do not match. Try again.";
-	}
-else 
-	{
-	echo "You've registered!";
-	}
 ?>
