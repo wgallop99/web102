fa316eee5bba79b12bd8d369fe3efd0fc0c3c4e9
ccravens <chad.cravens@ossys.com>
2014-06-23 17:02:47 -0400

    fixed search

--- php/search.php
+++ php/search.php
@@ -1,7 +1,7 @@
 <?php
-        $last_name = $_GET['lastname'];
+        $search = $_GET['search'];
 
-        $email = "";
+        $json = "";
         
         $con = mysql_connect("iweb.ossys.com","web102","PUR5tr3ngth","web102");
         if (mysqli_connect_errno()) {
@@ -9,14 +9,25 @@
         }
         mysql_select_db("web102");
 
-        $result = mysql_query("SELECT email FROM user WHERE last_name LIKE '%$last_name%'");
+        $result = mysql_query("SELECT * FROM alien WHERE name LIKE '%$search%' OR address LIKE '%$search%'");
         if(!$result) {
                 echo mysql_error();
         } else {
+        		$json .= "{ 'result' : [";
                 while($row = mysql_fetch_array($result)){
-                        $email = $row['email'];
+        				$json .= "{";
+                        $json .= "'name' : " . $row['name'] . ",";
+                        $json .= "'age' : " . $row['age'] . ",";
+                        $json .= "'species' : " . $row['species'] . ",";
+                        $json .= "'planet' : " . $row['planet'] . ",";
+                        $json .= "'star' : " . $row['star'] . ",";
+                        $json .= "'duration' : " . $row['duration'] . ",";
+                        $json .= "'address' : " . $row['address'] . ",";
+                		$json .= "}";
                 }
+                $json .= "]}";
         }
 
-        echo $email;
+		header('Content-Type: application/json');
+        echo $json;
 ?>
